4fbc8fcf95665f6307e8b961bd5af7d27f037d29
Brian Vo <datvo2k@gmail.com>
2023-12-15 00:40:07 +0700

    update

--- scenario/eks-self-managed/lib/eks-self-managed-stack.ts
+++ scenario/eks-self-managed/lib/eks-self-managed-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Vpc, InstanceType, SubnetType } from 'aws-cdk-lib/aws-ec2';
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
-import { Cluster, KubernetesVersion, NodeType, EksOptimizedImage } from 'aws-cdk-lib/aws-eks';
-import {AutoScalingGroup, UpdatePolicy} from 'aws-cdk-lib/aws-autoscaling'
+import { Vpc, InstanceType, SubnetType } from "aws-cdk-lib/aws-ec2";
+import { Role, ServicePrincipal, ManagedPolicy } from "aws-cdk-lib/aws-iam";
+import {
+  Cluster,
+  KubernetesVersion,
+  NodeType,
+  EksOptimizedImage,
+} from "aws-cdk-lib/aws-eks";
+import {
+  AutoScalingGroup,
+  UpdatePolicy,
+  BlockDeviceVolume,
+  EbsDeviceVolumeType,
+} from "aws-cdk-lib/aws-autoscaling";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EksSelfManagedStack extends cdk.Stack {
@@ -16,26 +26,39 @@ export class EksSelfManagedStack extends cdk.Stack {
     const workerRole = new Role(this, "EKSWorkerRole", {
       assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
     });
-    workerRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSClusterPolicy'))
-    workerRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSServicePolicy'))
-    
+    workerRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSClusterPolicy")
+    );
+    workerRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSServicePolicy")
+    );
+
     const eksCluster = new Cluster(this, "EKS-demo", {
       vpc: vpc,
       defaultCapacity: 0, // we want to manage capacity our selves
       version: KubernetesVersion.V1_28,
     });
 
-    const onDemandASG = new AutoScalingGroup(this, 'OnDemandASG', {
+    const onDemandASG = new AutoScalingGroup(this, "OnDemandASG", {
       vpc: vpc,
       role: workerRole,
       minCapacity: 1,
       maxCapacity: 5,
-      instanceType: new InstanceType('t3.medium'),
+      instanceType: new InstanceType("t3.small"),
       machineImage: new EksOptimizedImage({
-        kubernetesVersion: '1.28',
-        nodeType: NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
+        kubernetesVersion: "1.28",
+        nodeType: NodeType.STANDARD, // without this, incorrect SSM parameter for AMI is resolved
       }),
-      updatePolicy: UpdatePolicy.rollingUpdate()
+      blockDevices: [
+        {
+          deviceName: "gp3-volume",
+          volume: BlockDeviceVolume.ebs(30, {
+            volumeType: EbsDeviceVolumeType.GP3,
+            throughput: 125,
+          }),
+        },
+      ],
+      updatePolicy: UpdatePolicy.rollingUpdate(),
     });
 
     eksCluster.connectAutoScalingGroupCapacity(onDemandASG, {});
