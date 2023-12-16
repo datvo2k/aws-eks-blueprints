a10a775f4102aba2d554076602ac4c030cd400a4
Brian Vo <datvo2k@gmail.com>
2023-12-17 00:50:58 +0700

    add terraform iam users and group

--- scenario/eks-self/outputs.tf
+++ scenario/eks-self/outputs.tf
@@ -97,25 +97,50 @@ output "cluster_primary_security_group_id" {
   value       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
 }
 
-# EKS Node Group Outputs - Public
-output "node_group_public_id" {
-  description = "Public Node Group ID"
-  value       = aws_eks_node_group.eks_ng_public.id
+// ----------------------------------------------------------------------------
+// EKS public nodegroup
+// ----------------------------------------------------------------------------
+# output "node_group_public_id" {
+#   description = "Public Node Group ID"
+#   value       = aws_eks_node_group.eks_ng_public.id
+# }
+
+# output "node_group_public_arn" {
+#   description = "Public Node Group ARN"
+#   value       = aws_eks_node_group.eks_ng_public.arn
+# }
+
+# output "node_group_public_status" {
+#   description = "Public Node Group status"
+#   value       = aws_eks_node_group.eks_ng_public.status
+# }
+
+# output "node_group_public_version" {
+#   description = "Public Node Group Kubernetes Version"
+#   value       = aws_eks_node_group.eks_ng_public.version
+# }
+
+// ----------------------------------------------------------------------------
+// EKS private nodegroup
+// ----------------------------------------------------------------------------
+output "node_group_private_id" {
+  description = "Node Group 1 ID"
+  value       = aws_eks_node_group.eks_ng_private.id
 }
 
-output "node_group_public_arn" {
-  description = "Public Node Group ARN"
-  value       = aws_eks_node_group.eks_ng_public.arn
+output "node_group_private_arn" {
+  description = "Private Node Group ARN"
+  value       = aws_eks_node_group.eks_ng_private.arn
 }
 
-output "node_group_public_status" {
-  description = "Public Node Group status"
-  value       = aws_eks_node_group.eks_ng_public.status
+output "node_group_private_status" {
+  description = "Private Node Group status"
+  value       = aws_eks_node_group.eks_ng_private.status
 }
 
-output "node_group_public_version" {
-  description = "Public Node Group Kubernetes Version"
-  value       = aws_eks_node_group.eks_ng_public.version
+output "node_group_private_version" {
+  description = "Private Node Group Kubernetes Version"
+  value       = aws_eks_node_group.eks_ng_private.version
 }
 
 // ----------------------------------------------------------------------------
